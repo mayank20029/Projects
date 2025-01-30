fc51fa7623ef3fc184005ab023bba57170ab699c
mayank20029 <mayank.biet30@gmail.com>
2025-01-30 16:43:08 +0530

    Add files via upload
    In this code we have predicted need for maintenance for an equipment based on IOT sensor data captured daily. 
    5 classification models - Logistic, SVM, Randon forest, Naive Bayes and ANN are compared for accuracy.

---
+++ Predictive maintenance model.py
@@ -0,0 +1,129 @@
+#Objective is to predict the failure of machine as binary output (0 or 1)
+#I will be comparing most frequently known classification models . i.e. Logistic, SVM (rbf), Random forest, Naive bayes, ANN 
+
+#*******************Model 1 - Logistic regression************************
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('C:/Users/GOELMA7/Downloads/maintenance dataset.csv')
+X = dataset.iloc[:, 2:-1].values
+y = dataset.iloc[:, -1].values
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Encoding the Dependent Variable
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
+#accuracy 94.4%
+
+
+#**********************Model 2 - SVM rbf*************************************
+# Training the Kernel SVM model on the Training set
+from sklearn.svm import SVC
+classifier1 = SVC(kernel = 'rbf', random_state = 0)
+classifier1.fit(X_train, y_train)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred1 = classifier1.predict(X_test)
+cm1 = confusion_matrix(y_test, y_pred1)
+print(cm1)
+accuracy_score(y_test, y_pred1)
+#accuracy 94.4%
+
+#************************model 3- random forest************************
+# Training the Random Forest Classification model on the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier2 = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier2.fit(X_train, y_train)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred2 = classifier2.predict(X_test)
+cm2 = confusion_matrix(y_test, y_pred2)
+print(cm2)
+accuracy_score(y_test, y_pred2)
+#accuracy 93.6%
+
+#************************model 4 Naive bayes**********************
+# Training the Naive Bayes model on the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier3 = GaussianNB()
+classifier3.fit(X_train, y_train)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred3 = classifier3.predict(X_test)
+cm3 = confusion_matrix(y_test, y_pred3)
+print(cm3)
+accuracy_score(y_test, y_pred3)
+#accuracy 5%
+
+
+#*************************model 5 ANN********************************
+import tensorflow as tf
+# Initializing the ANN
+ann = tf.keras.models.Sequential()
+
+# Adding the input layer and the first hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Adding the second hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units=4, activation='softmax'))
+
+# Part 3 - Training the ANN
+
+# Compiling the ANN
+ann.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+
+# Training the ANN on the Training set
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+# Predicting the Test set results
+y_pred4 = ann.predict(X_test)
+predicted_labels = np.argmax(y_pred4, axis=1)
+print(predicted_labels)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score
+cm4 = confusion_matrix(y_test, predicted_labels)
+print(cm4)
+accuracy_score(y_test, predicted_labels)
+#accuracy 94.4%
